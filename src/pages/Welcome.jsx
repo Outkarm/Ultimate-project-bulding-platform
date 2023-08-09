fe7ce3be4d3ff3f800ecb08267a08d9bb9465cba
Outkarm <jkpordje@gmail.com>
2023-08-09 21:53:48 +0000

    add home page to welcome

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,11 +1,12 @@
-import React from "react";
 import NavBar from "../components/NavBar";
+import HomePageBody from "../components/HomePageBody";
 import "../assets/stylesheet/style.css";
 
 const Welcome = () => {
   return (
     <div className="welcome-cont">
       <NavBar />
+      <HomePageBody />
     </div>
   );
 };
